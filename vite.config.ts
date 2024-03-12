31bc8c1672362d1c66fe4bad6560a1f227e7bf5f
mujtabaAli96 <48852703+mujtabaAli96@users.noreply.github.com>
2024-03-13 00:38:07 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://pizzabar.vercel.app/restaurant/pizzabar",
+  // base:"https://iamkashi.com/pizabar/",
 });
