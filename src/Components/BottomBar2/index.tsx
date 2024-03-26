8576c3de4223e8d709c9687906cfd31426bd9eaf
mujtabaAli96 <48852703+mujtabaAli96@users.noreply.github.com>
2024-03-27 00:16:24 +0500

    sc

--- src/Components/BottomBar2/index.tsx
+++ src/Components/BottomBar2/index.tsx
@@ -48,7 +48,7 @@ export default function BottomBar2({ changeMenu, selectedMenu }: appProps) {
         // background: "transparent",
         width: "100%",
         position: "fixed",
-        bottom: "55px",
+        bottom: "52px",
         height: "8vh",
         color: "white",
         display: "flex",
